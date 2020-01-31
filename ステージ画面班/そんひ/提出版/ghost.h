f26955ac79a3914cc5e11608febb00dae7700b96
sonson469 <sonson.owata@icloud.com>
2020-01-31 17:09:05 +0900

    新しいフォルダ

---
+++ ステージ画面班/そんひ/提出版/ghost.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include"direct3d.h"
+#include"vertex.h"
+
+// オブジェクト数
+#define NUMBER_OF_GHOST (10)
+// サイズ
+#define HORIZONTAL_SIZE_GHOST	(4.0f)
+#define VERTICAL_SIZE_GHOST		(4.0f)
+// スピード
+#define SPEED_GHOST (0.1f)
+// 衝突半径
+#define RADIUS_GHOST (HORIZONTAL_SIZE_GHOST / 2)
+
+class cGhost
+{
+public:
+
+	// オブジェクト
+	static cGhost* objects[NUMBER_OF_GHOST];
+
+	// 頂点
+	static LPDIRECT3DVERTEXBUFFER9	pVertexBuffer;
+	static VERTEX_3D*				pVertex;
+	static void SetVertex();
+
+	// 使用
+	bool use;
+	static void Set(D3DXVECTOR3 _position, float _width_move);
+	cGhost(D3DXVECTOR3 _position, float _width_move);
+
+	// パラメータ
+	float width_move;
+	D3DXVECTOR3 settingPosition;
+	int moveDirection;
+	static bool collision;
+
+	// 描画
+	D3DXMATRIX	worldMatrix;
+	D3DXVECTOR3	position;
+	D3DXVECTOR3	rotation;
+	D3DXVECTOR3	scale;
+
+	// 基本関数
+	static void Init();
+	static void Uninit();
+	static void Update();
+	static void Draw();
+
+	void Move();
+	void CollisionInspection();
+};
---
+++ ステージ画面班/そんひ/提出版/ground.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include"direct3d.h"
+#include"vertex.h"
+
+// ボードの数(上限)
+#define NUMBER_OF_GROUND (100)
+// ボードの縦横のサイズ(初期値)
+#define GROUND_VERTICAL_SIZE	(1.0f)
+#define GROUND_HORIZONTAL_SIZE	(1.0f)
+
+class cGround
+{
+public:
+
+	static cGround*				objects[NUMBER_OF_GROUND];
+
+	static LPDIRECT3DVERTEXBUFFER9	pVertexBuffer;
+	static VERTEX_3D*				pVertex;
+
+	// 使用
+	bool use;
+
+	// 描画
+	D3DXVECTOR3	position;
+	D3DXVECTOR3	rotation;
+	D3DXVECTOR3	scale;
+	D3DXMATRIX	worldMatrix;
+
+	// コンストラクタ
+	cGround(D3DXVECTOR2 _position, D3DXVECTOR2 _size);
+
+	// 頂点
+	static void SetVertex();
+
+	// 基本関数
+	static void Init();
+	static void Uninit();
+	static void Update();
+	static void Draw();
+
+	// 配置
+	static void SetGround(D3DXVECTOR2 _position, D3DXVECTOR2 _size);
+};
